2f7a578deb0ad72d20ec9358664709c5170d33f5
Jason Woo <jaswoo73@gmail.com>
2024-02-01 20:59:11 +0800

    first-commit

---
+++ src/Menu.jsx
@@ -0,0 +1,12 @@
+import MenuItem from "./MenuItem";
+
+const Menu = ({ menu }) => {
+  return (
+    <div className="section-center">
+      {menu.map((menuItem) => {
+        return <MenuItem key={menuItem.id} {...menuItem} />;
+      })}
+    </div>
+  );
+};
+export default Menu;
